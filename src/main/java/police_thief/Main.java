a00060bdeb83b1a539536612ad790158ecdde278
seonjiwon <tommys915@gmail.com>
2026-02-12 16:01:01 +0900

    fix/police-move

--- src/main/java/police_thief/Main.java
+++ src/main/java/police_thief/Main.java
@@ -24,10 +24,10 @@ public class Main {
         // 3. 경찰 생성
         // 순찰 구역은 맵 전체(0~20)로 설정하고, 시작 위치는 (0,0)으로 지정합니다.
         List<Police> polices = new ArrayList<>();
-        polices.add(new Police(1, thieves, 0, 0, 10, 10));    // 좌상
-        polices.add(new Police(2, thieves, 11, 0, 20, 10));   // 우상
-        polices.add(new Police(3, thieves, 0, 11, 10, 20));   // 좌하
-        polices.add(new Police(4, thieves, 11, 11, 20, 20));  // 우하
+        polices.add(new Police(1, thieves, 0, 10, 0, 10));    // 좌상
+        polices.add(new Police(2, thieves, 11, 20, 0, 10));   // 우상
+        polices.add(new Police(3, thieves, 0, 10, 11, 20));   // 좌하
+        polices.add(new Police(4, thieves, 11, 20, 11, 20));  // 우하
 
         
 
